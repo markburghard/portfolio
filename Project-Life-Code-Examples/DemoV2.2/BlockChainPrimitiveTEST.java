9bbac5e366a1411201390eb04769ba728febf1f6
markburghard <markromanburghard@gmail.com>
2023-08-18 19:03:37 -0700

    Updated README and added Project LIFE examples.
    Added some sections of code from Project LIFE, a blockchain-based medical records software I built with some classmates for my capstone project at DePaul University.

---
+++ Project-Life-Code-Examples/DemoV2.2/BlockChainPrimitiveTEST.java
@@ -0,0 +1,23 @@
+package demo2.V2;
+
+public class BlockChainPrimitiveTEST {
+
+	public static void main(String[] args) throws Exception {
+		
+		BlockChainPrimitive LIFEChain = new BlockChainPrimitive();
+		HashAlgorithm HA = new HashAlgorithm();
+		
+		String file1 = FileReadIn.readInFile("C:/Users/mburg/Desktop/Capstone Project/SampleMedicalTransaction.xml");
+		String file2 = FileReadIn.readInFile("C:/Users/mburg/Desktop/Capstone Project/SampleMedicalTransaction2.xml");
+		
+		StringBuffer transaction1 = HA.Hash(file1);
+		StringBuffer transaction2 = HA.Hash(file2);
+		
+		LIFEChain.addBlock(transaction1);
+		LIFEChain.addBlock(transaction2);
+			
+
+		System.out.println(LIFEChain.ToString());
+	}
+
+}
--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # portfolio
  Programming portfolio with code examples and demos.
+
+Thank you for taking the time to look through this programming portfolio! This contains some examples of code that I've written. 
+It's a loose collection of projects and code sections to give you an idea of the kind of code that I write.
+
+I'll be adding to this with personal projects when I can. Feel free to peruse what's here!
+
+Best Regards,
+Mark Burghard
